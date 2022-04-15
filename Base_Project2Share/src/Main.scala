913912519078f389a6bfbd66c123281e10f753e7
Tânia <102622422+tecgo@users.noreply.github.com>
2022-04-15 19:13:25 +0100

    T4 Octree

--- Base_Project2Share/src/Main.scala
+++ Base_Project2Share/src/Main.scala
@@ -1,3 +1,4 @@
+import Octree.{Placement, Section, auxScale}
 import Octree1.Octree1.partitionId
 import Octree1.root
 import com.sun.javafx.scene.shape.ShapeHelper.ShapeAccessor
@@ -174,11 +175,6 @@ class Main extends Application {
     val ocLeaf1 = OcLeaf(sec1)
     val oct1:Octree[Placement] = OcNode[Placement](placement1, ocLeaf1, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty)
 
-    val list = oct1.toString.toList
-    val list1 = oct1.toString.split(",").toList
-    println("Oct1: " + list1)
-    println(list1(0)+ " " + list1(1))
-
     //example of bounding boxes (corresponding to the octree oct1) added manually to the world
     val b2 = new Box(8,8,8)
     //translate because it is added by default to the coords (0,0,0)
@@ -239,11 +235,34 @@ object FxApp {
   def main(args: Array[String]): Unit = {
     //------- Area de Testes -------
     //IO_Utils.readFromFile(s"$args")
-    println("Partition: " + partitionId(root).toString())
+    //println("Partition: " + partitionId(root).toString())
+    val placement1: Placement = ((0, 0, 0), 8.0)
+    val cylinder1 = new Cylinder(0.5, 1, 10)
+    cylinder1.setTranslateX(2)
+    cylinder1.setTranslateY(2)
+    cylinder1.setTranslateZ(2)
+    cylinder1.setScaleX(2)
+    cylinder1.setScaleY(2)
+    cylinder1.setScaleZ(2)
+    val greenMaterial = new PhongMaterial()
+    greenMaterial.setDiffuseColor(Color.rgb(0,255,0))
+    cylinder1.setMaterial(greenMaterial)
 
+    val box1 = new Box(1, 1, 1)
+    box1.setTranslateX(5)
+    box1.setTranslateY(5)
+    box1.setTranslateZ(5)
+    box1.setMaterial(greenMaterial)
+    box1.getBoundsInParent
+    val sec1: Section = (((0.0,0.0,0.0), 4.0), List(cylinder1.asInstanceOf[Node], box1.asInstanceOf[Node]))
+    val ocLeaf1 = OcLeaf(sec1)
+    val oct1:Octree[Placement] = OcNode[Placement](placement1, ocLeaf1, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty)
+    auxScale(2, oct1)
     //------------------------------
     Application.launch(classOf[Main], args: _*)
 
   }
+
+
 }
 
--- Base_Project2Share/src/Octree.scala
+++ Base_Project2Share/src/Octree.scala
@@ -1,27 +1,72 @@
-import scala.collection.StepperShape.Shape
+import javafx.scene.Node
 
 sealed trait Octree[+A]
 
-case class OcNode[A](coords: A,
+case class OcNode[A](placement: A,
                     up_00: Octree[A], up_01: Octree[A],
                      up_10: Octree[A], up_11: Octree[A],
                      down_00: Octree[A], down_01: Octree[A],
                      down_10: Octree[A], down_11: Octree[A]
                     ) extends Octree[A]
 
-
 case class OcLeaf[A, B](section: B) extends Octree[A]
 
 case object OcEmpty extends Octree[Nothing]
 
-//case class OcTreeShapes[Placement] extends Octree[Placement] {
-//    def createTree():OcTreeShapes[Placement] = {
-//
-//    }
-//}
-//
-//object OcTreeShapes{
-//  def createTree():OcTreeShapes[Placement] = {
-//
-//  }
-//}
+case object OctreeUtils extends Octree[Nothing]
+
+  object Octree {
+
+    type Point = (Double, Double, Double)
+    type Size = Double
+    type Placement = (Point, Size) //1st point: origin, 2nd point: size
+    type Section = (Placement, List[Node])
+
+    def auxScale(fact: Double, oct: Octree[Placement]): (Octree[Placement]) = {
+      val octSize = oct.asInstanceOf[OcNode[Placement]].placement._2
+      val octScale = octSize * fact
+
+      val list_ocNodes = List() :+ oct.asInstanceOf[OcNode[Placement]].up_00 :+ oct.asInstanceOf[OcNode[Placement]].up_01 :+
+        oct.asInstanceOf[OcNode[Placement]].up_10 :+ oct.asInstanceOf[OcNode[Placement]].up_11 :+ oct.asInstanceOf[OcNode[Placement]].down_00 :+
+        oct.asInstanceOf[OcNode[Placement]].down_01 :+ oct.asInstanceOf[OcNode[Placement]].down_10 :+ oct.asInstanceOf[OcNode[Placement]].down_11
+
+      // Da lista de nodes, tirar aqueles que sao ocLeafs
+      val list_ocLeaf = list_ocNodes.filter(x => x.isInstanceOf[OcLeaf[Octree[Section], Section]])
+      //println("lista_ocLeaf_auxScale ------>" + list_ocLeaf)
+      val shapes_list : List[Node] = List()
+      //Se a lista de ocLeafs não estiver vazia, vamos percorrê-la
+      if (list_ocLeaf.nonEmpty) {
+
+        list_ocLeaf.foldRight(0)((h, t) => {
+          //Criar lista de modelos graficos da ocleaf
+          shapes_list :: h.asInstanceOf[OcLeaf[Octree[Section], Section]].section._2
+          //println("Shapes_list1: " + shapes_list)
+
+          //Percorrer lista de modelos graficos da ocLeaf
+          shapes_list.foldRight(0)((h, t) => {
+            println("shape before: " + h.getScaleX + " " + h.getScaleY + " " + h.getScaleZ)
+            h.setScaleX(h.getScaleX*fact)
+            h.setScaleY(h.getScaleY * fact)
+            h.setScaleY(h.getScaleZ * fact)
+            println("shape after: " + h.getScaleX + " " + h.getScaleY + " " + h.getScaleZ)
+            ;t
+          });t
+        })
+
+        val placement1: Placement = (oct.asInstanceOf[OcNode[Placement]].placement._1, octScale)
+        val sec1: Section = (placement1, shapes_list)
+        val ocLeaf1 = OcLeaf(sec1)
+        val scaleOctree:Octree[Placement] = OcNode[Placement](placement1, ocLeaf1,list_ocNodes(1), list_ocNodes(2), list_ocNodes(3), list_ocNodes(4),
+          list_ocNodes(5), list_ocNodes(6), list_ocNodes(7))
+
+        scaleOctree
+      }
+      else {
+        val placement1: Placement = (oct.asInstanceOf[OcNode[Placement]].placement._1,octScale)
+        val scaleOctree:Octree[Placement] = OcNode[Placement](placement1, list_ocNodes(0),list_ocNodes(1), list_ocNodes(2), list_ocNodes(3), list_ocNodes(4),
+          list_ocNodes(5), list_ocNodes(6), list_ocNodes(7))
+        scaleOctree
+      }
+    }
+
+}

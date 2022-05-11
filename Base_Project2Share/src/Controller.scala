9ea19ccf9f89e7eef839791f829cf528802c203f
Tânia <102622422+tecgo@users.noreply.github.com>
2022-05-11 22:38:05 +0100

    Botoes GUI Version1

--- Base_Project2Share/src/Controller.scala
+++ Base_Project2Share/src/Controller.scala
@@ -1,7 +1,13 @@
+import IO_Utils.oct
+import OctreeUtils.mapColourEffect
 import javafx.fxml.FXML
-import javafx.scene.control.Button
+import javafx.scene.control.{Button, RadioButton, ToggleButton, ToggleGroup}
 import javafx.scene.{Group, SubScene}
 import javafx.scene.layout.{AnchorPane, GridPane}
+import javafx.fxml.FXML
+import javafx.scene.control.ToggleGroup
+
+import java.awt.event.MouseEvent
 
 class Controller {
 
@@ -12,6 +18,21 @@ class Controller {
   @FXML
   private var anchorPane1:AnchorPane = _
 
+  @FXML
+  private var button_color : Button = _
+  @FXML
+  private var button_scale : Button = _
+  @FXML
+  private var button_visible : ToggleButton = _
+  @FXML
+  private var button_sepia : RadioButton = _
+  @FXML
+  private var button_greenRemove : RadioButton = _
+  @FXML
+  private var button_fator2 : RadioButton = _
+  @FXML
+  private var button_fator05 : RadioButton = _
+
   //method automatically invoked after the @FXML fields have been injected
   @FXML
   def initialize(): Unit = {
@@ -21,8 +42,34 @@ class Controller {
     subScene1.heightProperty.bind(anchorPane1.heightProperty)
     subScene1.setRoot(InitSubScene.root)
 
-//    InitSubScene.worldRoot.getChildren.add(OctreeUtils.SPACE_LIMIT)
+
+    //    InitSubScene.worldRoot.getChildren.add(OctreeUtils.SPACE_LIMIT)
     OctreeUtils.addOctreeToWorldRoot(IO_Utils.oct, InitSubScene.worldRoot)
+  }
+    def onButtonClicked_color():Unit = {
+
+      if (button_sepia.isSelected) OctreeUtils.mapColourEffect(c => OctreeUtils.sepiaEffect(c))(IO_Utils.oct)
+      else if (button_greenRemove.isSelected) OctreeUtils.mapColourEffect(c => OctreeUtils.greenRemove(c))(IO_Utils.oct)
+    }
+
+  //def onButtonClicked_scale(): Octree[((Double, Double, Double), OctreeUtils.Size)] = {
+    //case button_fator2.isSelected => button_scale.setOnMouseClicked(e=>OctreeUtils.scaleOctree(2, IO_Utils.oct))
+    //case button_fator05.isSelected  => button_scale.setOnMouseClicked(e=>OctreeUtils.scaleOctree(0.5, IO_Utils.oct))
+  //}
+
+
+  def onButtonClicked_scale():Unit = {
+
+    //println("Botao 0.5: " + button_fator05.isSelected.toString)
+    //println("Botao 2: " + button_fator2.isSelected.toString)
+
+    if (button_fator2.isSelected) OctreeUtils.scaleOctree(2, IO_Utils.oct)
+    else if(button_fator05.isSelected) OctreeUtils.scaleOctree(0.5, IO_Utils.oct)
 
   }
+
+    //def onButtonClicked_visible():Unit = {
+      //button_visible
+    //}
+
 }

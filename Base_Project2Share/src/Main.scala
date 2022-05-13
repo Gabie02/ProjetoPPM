69904250399144e17eaedf5bfa82eadfbfc95231
Gabie02 <gpjle@iscte-iul.pt>
2022-05-13 18:03:45 +0100

    Alterações quase finais

--- Base_Project2Share/src/Main.scala
+++ Base_Project2Share/src/Main.scala
@@ -6,13 +6,6 @@ import javafx.stage.Stage
 class Main extends Application {
 
   override def start(stage: Stage): Unit = {
-
-    //Get and print program arguments (args: Array[String])
-//        val params = getParameters
-//        println("Program arguments:" + params.getRaw)
-
-//    Se o utilizador selecionou para escolher o ficheiro, usar a octree dada pelo utils, caso contrário fazer a arvore aqui (com o atributo da main?)
-//    if (IO_Utils)
     stage.setTitle("Projeto PPM 21/22")
     val fxmlLoader = new FXMLLoader(getClass.getResource("Controller.fxml"))
     val mainViewRoot: Parent = fxmlLoader.load()
@@ -22,11 +15,11 @@ class Main extends Application {
   }
 
   override def init(): Unit = {
-    println("init")
+
   }
 
   override def stop(): Unit = {
-    println("stopped")
+
   }
 }
 
--- Base_Project2Share/src/OctreeUtils.scala
+++ Base_Project2Share/src/OctreeUtils.scala
@@ -6,9 +6,6 @@ import scala.annotation.tailrec
 
 object OctreeUtils {
 
-  //Wiredbox que limita o espaço 3D
-//  val SPACE_LIMIT: Shape3D = OctreeUtils.createWiredBox((0, 0, 0), 32)
-
   //Auxiliary types
   type Point = (Double, Double, Double)
   type Size = Double
@@ -20,18 +17,18 @@ object OctreeUtils {
     (p1._1 + p2._1, p1._2 + p2._2, p1._3 + p2._3)
   }
 
-  def getAllShapes(oct: Octree[Placement]):List[Node] = oct match {
+  def getAllShapes(oct: Octree[Placement]):List[Shape3D] = oct match {
     case n: OcNode[Placement] =>
       val atributos = createAttributesList(n)
-      (atributos foldRight List[Node]()) ((h, t) => {
+      (atributos foldRight List[Shape3D]()) ((h, t) => {
         getAllShapes(h)++t
       })
     case l: OcLeaf[Placement, Section] =>
-      l.section._2
-    case _ => List[Node]()
+      l.section._2.asInstanceOf[List[Shape3D]]
+    case _ => List[Shape3D]()
   }
 
-  /*  Devolve os todos os atributos de um node, exceto o primeiro, que é o placement */
+  //Devolve os todos os atributos de um node, exceto o primeiro, que é o placement
   def createAttributesList(e: OcNode[Placement]): List[Octree[Placement]] = {
     val numAtributos = e.productArity
 
@@ -43,30 +40,14 @@ object OctreeUtils {
 
     iterate(e, List[Octree[Placement]](), 1)
   }
-
-  //Cria uma wiredBox com o tamanho desejado
-  def createWiredBox(origin: Point, size: Size): Shape3D = {
-    val box = new Box(size, size, size)
-    box.setTranslateX(origin._1)
-    box.setTranslateY(origin._2)
-    box.setTranslateZ(origin._3)
-    //    box.setMaterial(InitSubScene.redMaterial)
-    val redMaterial = new PhongMaterial()
-    redMaterial.setDiffuseColor(Color.rgb(150,0,0))
-    box.setMaterial(redMaterial)
-    box.setDrawMode(DrawMode.LINE)
-    box
-  }
-
+  //Adiciona a tree à WorldRoot
   def addOctreeToWorldRoot(oct: Octree[Placement], worldRoot: Group): Unit = oct match {
-
     case n: OcNode[Placement] =>
       val atributos = createAttributesList(n)
       (atributos foldRight()) ((h, t) => {
         addOctreeToWorldRoot(h, worldRoot)
         t
       })
-
     case l: OcLeaf[Placement, Section] =>
       val listShapes = l.section._2
 
@@ -74,13 +55,25 @@ object OctreeUtils {
         worldRoot.getChildren.add(h)
         t
       })
-
     case _ =>
   }
-
   //T2 Auxiliary functions
 
-  /*  Devolve uma octree igual, mas com a tree (element) dada na posição indicada pelo index */
+  //Cria uma wiredBox com o tamanho desejado
+  def createWiredBox(origin: Point, size: Size): Shape3D = {
+    val box = new Box(size, size, size)
+    box.setTranslateX(origin._1)
+    box.setTranslateY(origin._2)
+    box.setTranslateZ(origin._3)
+    val redMaterial = new PhongMaterial()
+    redMaterial.setDiffuseColor(Color.rgb(150,0,0))
+    box.setMaterial(redMaterial)
+    box.setDrawMode(DrawMode.LINE)
+    box
+  }
+
+
+  //Devolve uma octree igual, mas com a tree (element) dada na posição indicada pelo index
   def putElementAt(tree: Octree[Placement], element: Octree[Placement], index: Int): Octree[Placement] = {
     val node = tree.asInstanceOf[OcNode[Placement]]
     index match {
@@ -109,16 +102,6 @@ object OctreeUtils {
       addTwoPoints(newPoint, (size / 4 + size / 2, size / 4 + size / 2, size / 4)) :+ addTwoPoints(newPoint, (size / 4 + size / 2, size / 4 + size / 2, size / 4 + size / 2))
   }
 
-  //É meio estúpido porque vai devolver o mesmo shape
-  def shapeInTheRightPosition(shape: Shape3D, partition: Shape3D):Shape3D = {
-    if(!partition.getBoundsInParent.contains(shape.getBoundsInParent)) {
-      shape.setTranslateX(partition.getTranslateX)
-      shape.setTranslateY(partition.getTranslateY)
-      shape.setTranslateZ(partition.getTranslateZ)
-    }
-    shape
-  }
-
   //Determina se um shape contido num node está contido numa partição do mesmo
   // (Se a partição pode ser dividida para esse node)
   def canBeDivided(node: Placement, s: Shape3D): Boolean = {
@@ -129,33 +112,9 @@ object OctreeUtils {
     })
   }
 
-  def canBeDivided2(node: Placement, s: Shape3D): Boolean = {
-    val partitionsOrigins = createOrigins(node)
-    //Cópia do shape, mas com uma origem diferente
-    //    val shapeCopy = s.clone().asInstanceOf[Shape3D]
-    val origin = (s.getTranslateX, s.getTranslateY, s.getTranslateZ)
-    (partitionsOrigins foldRight false) ((h, t) => {
-      s.setTranslateX(h._1)
-      s.setTranslateY(h._2)
-      s.setTranslateZ(h._3)
-      //      val b = Bounds()
-      val partition = createWiredBox(h, node._2 / 2)
-      if (partition.getBoundsInParent.contains(s.getBoundsInParent)) {
-        s.setTranslateX(origin._1); s.setTranslateY(origin._2); s.setTranslateZ(origin._3)
-        true
-      } else{
-        s.setTranslateX(origin._1); s.setTranslateY(origin._2); s.setTranslateZ(origin._3)
-        t
-      }
-    })
-  }
-
   // --- T2 ---
-
-  //Cria um octree sem especificar o tamanho da root (Tem como root o cubo que limita o espaço)
-  def createTree(shapeList: List[Shape3D]): Octree[Placement] = createTree(shapeList, ((16.0, 16.0, 16.0), 32))
-
-  def createTree(shapeList: List[Shape3D], root: Placement): Octree[Placement] = {
+  //Cria um octree (Tem como root default o cubo que limita o espaço)
+  def createTree(shapeList: List[Shape3D], root: Placement = ((16.0, 16.0, 16.0), 32)): Octree[Placement] = {
     val size = root._2
     val emptyOcNode = new OcNode[Placement](((0.0, 0.0, 0.0), size / 2), OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty, OcEmpty)
     val partitionsOrigins = createOrigins(root)
@@ -186,11 +145,8 @@ object OctreeUtils {
             case value: OcLeaf[Placement, Section] =>
               val list = value.section._2
 
-              val shape = shapeInTheRightPosition(h, partition)
-
               //retorna uma nova árvore com a lista atualizada e o elemento, mais a partição, no seu sitio
-              //              val finalTree = putElementAt(tree, new OcLeaf[Placement, Section]((partitions(i), size / 2), list :+ h :+ partition), i).asInstanceOf[OcNode[Placement]]
-              val finalTree = putElementAt(tree, new OcLeaf[Placement, Section]((partitions(i), size / 2), list :+ shape :+ partition), i).asInstanceOf[OcNode[Placement]]
+              val finalTree = putElementAt(tree, new OcLeaf[Placement, Section]((partitions(i), size / 2), list :+ h :+ partition), i).asInstanceOf[OcNode[Placement]]
               //Iterar para a próxima partição
               return iterateThroughPartitions(finalTree, partitions, i + 1, stop)
 
@@ -216,7 +172,6 @@ object OctreeUtils {
 
   //  --- T3 ---
   def intersectsCamera(oct:Octree[Placement], camVolume: Cylinder):Unit = {
-
     oct match {
       // Se for um ocNode
       case _: OcNode[Placement] =>
@@ -235,21 +190,22 @@ object OctreeUtils {
         // Se a lista de nodes da section nao estiver vazia
         listaSection.foldRight(0)((h, t) => {
           h.asInstanceOf[Shape3D]
-          if (h.isInstanceOf[Box]) {
-            val box = h.asInstanceOf[Box]
-            //Verifica se é uma wiredBox?
-            if (box.getDrawMode.equals(DrawMode.LINE)) {
-              // Se intersetar
-              if (camVolume.asInstanceOf[Shape3D].getBoundsInParent.intersects(box.getBoundsInParent)) {
-                val greenMaterial = new PhongMaterial()
-                greenMaterial.setDiffuseColor(Color.rgb(0, 255, 0))
-                box.setMaterial(greenMaterial)
-              } else {
-                val redMaterial = new PhongMaterial()
-                redMaterial.setDiffuseColor(Color.rgb(150, 0, 0))
-                box.setMaterial(redMaterial)
+          h match {
+            case box: Box =>
+              //Verifica se é uma wiredBox?
+              if (box.getDrawMode.equals(DrawMode.LINE)) {
+                // Se intersetar
+                if (camVolume.asInstanceOf[Shape3D].getBoundsInParent.intersects(box.getBoundsInParent)) {
+                  val greenMaterial = new PhongMaterial()
+                  greenMaterial.setDiffuseColor(Color.rgb(0, 255, 0))
+                  box.setMaterial(greenMaterial)
+                } else {
+                  val redMaterial = new PhongMaterial()
+                  redMaterial.setDiffuseColor(Color.rgb(150, 0, 0))
+                  box.setMaterial(redMaterial)
+                }
               }
-            }
+            case _ =>
           }
           t
         })
@@ -260,38 +216,33 @@ object OctreeUtils {
 
   //  --- T4 ---
   def scaleOctree(fact: Double, oct: Octree[Placement]): Octree[Placement] = fact match {
-    case 0.5 | 2 => auxScale(fact, oct)
+    case 0.5 | 2 =>
+      val listaDeShapes = getAllShapes(oct)
+
+      (listaDeShapes foldRight ())((h,_) => {
+
+        h.setTranslateZ(h.getTranslateZ * fact)
+        h.setTranslateX(h.getTranslateX * fact)
+        h.setTranslateY(h.getTranslateY * fact)
+
+        h.setScaleX(h.getScaleX * fact)
+        h.setScaleY(h.getScaleY * fact)
+        h.setScaleZ(h.getScaleZ * fact)
+      })
+      val shapeListWithoutPartitions = listaDeShapes.filterNot(s => s.isInstanceOf[Box] && (s.asInstanceOf[Box].getDrawMode == DrawMode.LINE))
+      createTree(shapeListWithoutPartitions)
+
     case _ => println("--> Fator inválido!!!"); throw new IllegalArgumentException("Argumento inválido: Não foi possível efetuar scale, factor inválido ")
   }
 
-  def auxScale(fact: Double, oct: Octree[Placement]): Octree[Placement] = {
-
-    val listaDeShapes = getAllShapes(oct)
-
-    (listaDeShapes foldRight ())((h,_) => {
-
-      h.setTranslateZ(h.getTranslateZ * fact)
-      h.setTranslateX(h.getTranslateX * fact)
-      h.setTranslateY(h.getTranslateY * fact)
-
-      h.setScaleX(h.getScaleX * fact)
-      h.setScaleY(h.getScaleY * fact)
-      h.setScaleZ(h.getScaleZ * fact)
-    })
-    oct
-  }
-
   //  --- T5 ---
-
   def sepiaEffect(c: Color): Color = {
     val r = c.getRed * 255
     val g = c.getGreen * 255
     val b = c.getBlue * 255
-    println(s"Cor: $c RGB: $r, $g, $b")
     val newR = if (0.4 * r + 0.77 * g + 0.2 * b > 255.0) 255 else 0.4 * r + 0.77 * g + 0.2 * b
     val newG = if (0.35 * r + 0.69 * g + 0.17 * b > 255.0) 255 else 0.35 * r + 0.69 * g + 0.17 * b
     val newB = if (0.27 * r + 0.53 * g + 0.13 * b > 255.0) 255 else 0.27 * r + 0.53 * g + 0.13 * b
-    println(s"New RGB: $newR, $newG, $newB")
     Color.rgb(newR.toInt, newG.toInt, newB.toInt)
   }
 
@@ -300,47 +251,14 @@ object OctreeUtils {
   }
 
   def mapColourEffect(func: Color => Color)(oct: Octree[Placement]): Octree[Placement] = {
-    val root = oct.asInstanceOf[OcNode[Placement]]
-    val inputSize = root.productArity
-
-    @tailrec
-    def iterate(root: OcNode[Placement], i: Int, stop: Int): Octree[Placement] = {
-
-      val partition = root.productElement(i)
-
-      partition match {
-        case _: OcNode[Placement] =>
-
-          mapColourEffect(func)(partition.asInstanceOf[Octree[Placement]])
-
-        case _: OcLeaf[Placement, Section] =>
-
-          val shapeList: List[Node] = partition.asInstanceOf[OcLeaf[Placement, Section]].section._2
-          (shapeList foldRight List[Node]()) ((h, t) => {
-            val material = h.asInstanceOf[Shape3D].getMaterial.asInstanceOf[PhongMaterial]
-            val color: Color = material.getDiffuseColor
-            h match {
-              case _: Box =>
-                val box = h.asInstanceOf[Box]
-                if (box.getDrawMode.equals(DrawMode.LINE)) {
-                  t
-                } else {
-                  material.setDiffuseColor(func(color))
-                  t
-                }
-              case _ =>
-                material.setDiffuseColor(func(color))
-                t
-            }
-          })
-
-        case _ =>
-      }
-      if (i < stop) iterate(root, i + 1, stop) else root
-    }
-
-    iterate(root, 0, inputSize - 1)
+    val leafsList = getAllShapes(oct)
+    val shapeList = leafsList.filterNot(s => s.isInstanceOf[Box] && (s.asInstanceOf[Box].getDrawMode == DrawMode.LINE))
+    (shapeList foldRight()) ((h, _) => {
+      val material = h.getMaterial.asInstanceOf[PhongMaterial]
+      val color: Color = material.getDiffuseColor
+      material.setDiffuseColor(func(color))
+    })
+    createTree(shapeList)
   }
-
 }
 
